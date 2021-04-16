4b4d110cc8ef29153aaea650093d12d84c068576
jinook929 <jinook929@gmail.com>
2021-04-16 19:32:39 -0400

    commit tree fc62a19f8d9e3572cde6ce800889572121dba77f

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const arr = ['Sally', 'Bob', 'Freddy', 'Claudia']
+
+const returnFirstTwoDrivers = function (arr) {
+  // const result = []
+  // for(let i = 0; i < 2; i++) {
+  //   result.push(arr[i])
+  // }
+  // return result
+  return arr.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function (arr) {
+  // const result = []
+  // for(let i = 2; i > 0; i--) {
+  //   result.push(arr[arr.length - i])
+  // }
+  // return result
+  return arr.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplingRate) {
+  return function(fare) {
+    return fare * multiplingRate
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, pickDrivers) {
+  return pickDrivers(drivers)
+}
